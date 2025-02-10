54f3b2d05cbd32a5df3bbd8454669f9092b08dfd
kenort666 <artem-1909@mail.ru>
2025-02-10 17:02:26 +0300

    :truck: chore: добавлена favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 const App = () => (
-  <>
+  <main>
     <header>
       This is header
     </header>
-
     <footer>
       This is footer
     </footer>
-  </>
+  </main>
+
 );
 
 export default App;
